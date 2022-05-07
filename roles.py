e792711aa23bb721263161fb404e828a9fe03c7b
Tigrejor <jorge.garvin@gmail.com>
2022-05-07 20:09:47 +0200

    First commit

---
+++ roles.py
@@ -0,0 +1,41 @@
+import discord
+import asyncio
+from discord.ext.commands import Cog, command, CheckFailure
+from discord.utils import get
+from config import Config
+
+config = Config()
+
+class Roles(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def cog_check(self, ctx):
+        if ctx.channel.id == config.roles_channel:
+            return True
+
+    @Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, CheckFailure):
+            print(error)
+
+    async def cog_command_error(self, ctx, error):
+        if isinstance(error, CheckFailure):
+            await ctx.send("Canal incorrecto", delete_after=5)
+            await asyncio.sleep(5)
+            await ctx.message.delete()
+        else:
+            print(error)
+
+    @command(hide=True)
+    async def role(self, ctx):
+        """Shows own roles"""
+        roles = ""
+        for r in ctx.author.roles:
+            print(r)
+            print('--')
+            roles += str(r) + " "
+        await ctx.send("```Tus roles son: " + roles + "```")
+
+def setup(bot):
+    bot.add_cog(Roles(bot))
